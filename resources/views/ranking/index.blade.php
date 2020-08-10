1b6e826674d3178464c4af6243915642a012ded4
”名前” <>
2020-08-10 11:28:18 +0000

    rankingページ修正(component引数利用により不要なblade削除)

--- resources/views/ranking/index.blade.php
+++ resources/views/ranking/index.blade.php
@@ -3,29 +3,22 @@
 @section('title', 'おすすめドラマ')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 mx-auto bg-white">
-                <h1>おすすめドラマ(総合評価ランキング)</h1>
-                <div class="row">
-                    <div class="col-md-4 float-right">
-                        {{ $scores->appends(request()->input())->links() }}
-                    </div>
-                    <div class="col-md-8">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-11 mx-auto">
-                        @foreach($scores as $score )
-                            @include('layouts.component.rankingindex')
-                        @endforeach
-                        
-                        <p class="float-right">
-                            {{ $scores->appends(request()->input())->links() }}
-                        </p>
-                    </div>
-                </div>
-            </div>
+{{-- search.result.indexとほぼ全て同じ。違いは「テーブルの参照元($socres,$dramas)」「並び替えの有無」「includeするときに渡す引数」 --}}
+<div class="col-12 mx-auto content-frame">
+    <h1 class="content-title">おすすめドラマ(総合評価ランキング)</h1>
+    <div class="row main-content m-0">
+        {{-- スマホ時のみ上部にペジネーション表示 --}}
+        <div class="col-12 not-pc">
+            {{ $scores->appends(request()->input())->links() }}
         </div>
     </div>
+    
+    <div class="col-md-11 mx-auto">
+        @foreach($scores as $score)
+            @include('layouts.component.dramaindex', ['drama' => $score->drama()->first()])
+        @endforeach
+        
+        {{ $scores->appends(request()->input())->links() }}
+    </div>
+</div>
 @endsection
