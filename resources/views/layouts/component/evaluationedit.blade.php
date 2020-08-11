d59c3cfe28a85e40296c2b752e1f84fe76bd560b
”名前” <>
2020-08-11 18:47:24 +0000

    レビュー編集ページのデザイン作成

--- resources/views/layouts/component/evaluationedit.blade.php
+++ resources/views/layouts/component/evaluationedit.blade.php
@@ -1,11 +1,11 @@
-{{-- drama.dramaID.review.createにて利用 --}}
+{{-- drama.dramaID.review.editにて利用 --}}
 
 <div class="form-group col-sm-6 mb-2 {{ $order }}">
     <label for="{{ $evaluation }}" class="control-label bg-evaluation">{{ $item }}の評価
         <select class="original-form-control" id="{{ $evaluation }}" name="{{ $evaluation }}">
-            <option value="">--</option>
+            <option value="" @if( $evaluation_value == null ) selected @endif>--</option>
             @for($i = 10; $i > 0; $i--)
-                <option value="{{ $i/2 }}" @if(old('{{ $evaluation }}')== $i/2) selected @endif>{{ sprintf('%.1f', $i/2) }}</option>
+                <option value="{{ $i/2 }}" @if(($evaluation_value)== $i/2 ) selected @endif>{{ sprintf('%.1f', $i/2) }}</option>
             @endfor
         </select>
     </label>
